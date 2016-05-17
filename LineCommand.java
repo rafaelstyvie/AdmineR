10bd69e4526ca5ed4bbcb3103e68e764ee378a30
rafaelstyvie <rafael_styvie@hotmail.com>
2016-05-16 22:43:37 -0300

    Create LineCommand.java

---
+++ LineCommand.java
@@ -0,0 +1,55 @@
+package principal;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LineCommand {
+
+public synchronized static String execCommand(final String commandLine) throws IOException {
+
+boolean success = false;
+String result;
+
+Process p;
+BufferedReader input;
+StringBuffer cmdOut = new StringBuffer();
+String lineOut = null;
+int numberOfOutline = 0;
+
+try {
+
+p = Runtime.getRuntime().exec(commandLine);
+
+input = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+while ((lineOut = input.readLine()) != null) {
+if (numberOfOutline > 0) {
+cmdOut.append("\n");
+}
+cmdOut.append(lineOut);
+numberOfOutline++;
+}
+
+input.close();
+
+result = cmdOut.toString();
+
+success = (p.waitFor() == 0);
+
+} catch (IOException e) {
+result = String.format("Falha ao executar comando %s. Erro: %s", commandLine, e.toString());
+} catch (InterruptedException e) {
+result = String.format("Falha ao executar comando %s. Erro: %s", commandLine, e.toString());
+}
+
+// Se não executou com sucesso, lança a falha
+if (!success) {
+throw new IOException(result);
+}
+
+return result;
+
+}
+
+}
